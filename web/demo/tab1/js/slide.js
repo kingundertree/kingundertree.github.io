2663215c5a378041f102eef8b4ad743867152ad3
shanxu <kingundertree@163.com>
2015-03-16 16:13:14 +0800

    add UITableView 重用

--- web/demo/tab1/js/slide.js
+++
@@ -1,9 +0,0 @@
-//tab js
-function setTab(cursel,n){
- for(i=1;i<=n;i++){
-  var menu=document.getElementById("tabtit"+i);
-  var con=document.getElementById("cont"+i);
-  if(menu!=null)
-  	menu.className=(i==cursel?"hoverLi":"");
-    con.style.display=(i==cursel?"block":"none");
- }}
